2101b1d3931bae75605b3bf9d88e1505124e5d06
Gabriel <gabrielmatheussousa@hotmail.com>
2025-04-24 09:34:07 -0300

    meu_funcao

--- conexao.php
+++ conexao.php
@@ -1,19 +1,19 @@
 <?php
 echo '<pre>';
 
-$host ='localhost:3307';
+$host ='localhost:3306';
 $user = 'root';
 $pass= '';
 $db ='db_login';
 
 $mysqli = new mysqli($host, $user, $pass, $db);
 
-// function foi(){
-//     global $mysqli;
-//     $query = $mysqli->query("SELECT senha from tb_user");
-//     $resultado = $query->fetch_assoc();
-//     return $resultado;
-// }
-// var_export(foi());
+function foi2(){
+    global $mysqli;
+    $query = $mysqli->query("SELECT senha from tb_user");
+    $resultado = $query->fetch_assoc();
+    return $resultado;
+}
+var_export(foi());
 
 echo '</pre>';
